988d002dc8e6c12b5eff1dd1d5ac56962cd2ea39
Jia Rong Gan <jiaronggan21@gmail.com>
2025-03-12 21:49:38 -0700

    Added auth into app file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import ShareBnB from './api';
+import useLocalStorage from './hooks/useLocalStorage';
 import { useState, useEffect } from "react";
 import { BrowserRouter } from 'react-router-dom';
+import {jwtDecode as decode} from "jwt-decode";
 
 import RoutesList from './RoutesList';
 import Navbar from './Navbar';
 
+export const TOKEN_STORAGE_ID = "sharebnb-token"
 /** App for ShareBnB.
  *
  * Props:
@@ -21,7 +24,12 @@ import Navbar from './Navbar';
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [properties, setProperties] = useState([]);
-  console.log('App properties state: ', properties);
+  const [token, setToken] = useLocalStorage(TOKEN_STORAGE_ID)
+  const [currentUser, setCurrentUser] = useState({
+    data: null,
+    infoLoaded: false
+  });
+
   /** getPropertiesOnMount: Executes search without a search term. */
 
   useEffect(function getPropertiesOnMount() {
@@ -29,8 +37,59 @@ function App() {
     search();
   }, []);
 
-  /** addProperty: Makes a request to API to add a new property. Recieves 
-   * form data and file upload from form. 
+  /** Get user data if token exists. */
+  useEffect(
+    function loadUserInfo() {
+      console.debug("App useEffect loadUserInfo", "token=", token);
+
+      async function getCurrentUser() {
+        if (token) {
+          try {
+            let { username } = decode(token)
+            ShareBnB.token = token;
+            let currentUser = await ShareBnB.getCurrentUser(username);
+
+            setCurrentUser({
+              infoLoaded: true,
+              data: currentUser
+            });
+          } catch (err) {
+            console.error("App loadUserInfo: problem loading", err);
+            setCurrentUser({
+              infoLoaded: true,
+              data: null
+            });
+          }
+        } else {
+          setCurrentUser({
+            infoLoaded: true,
+            data: null
+          });
+        }
+      }
+      getCurrentUser();
+    },
+    [token]
+  );
+
+  /** Handles site-wide logout. */
+  function logout() {
+    setCurrentUser({
+      infoLoaded: true,
+      data: null
+    });
+    setToken(null);
+  }
+
+  /** Handles site-wide login. */
+  async function login(loginData) {
+    let token = await ShareBnB.login(loginData);
+    setToken(token)
+  }
+
+
+  /** addProperty: Makes a request to API to add a new property. Recieves
+   * form data and file upload from form.
    */
 
   async function addProperty(formData, file){
